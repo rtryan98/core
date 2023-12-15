491cc20981551dc9cb02e4f145d4ccec3ffd8b2f
Robert <33040161+rtryan98@users.noreply.github.com>
2023-12-15 07:30:19 +0100

    Use swapchain and window and resource manager

--- src/core_sample_framework/sample_base_app.cpp
+++ src/core_sample_framework/sample_base_app.cpp
@@ -6,28 +6,34 @@
 namespace core::sf
 {
 Sample_Application::Sample_Application(const Sample_Application_Create_Info& create_info)
-    : m_running(false)
-    , m_window(nullptr)
+    : m_window(nullptr)
     , m_d3d12_context()
-    , m_resource_manager()
+    , m_swapchain(nullptr)
+    , m_resource_manager(nullptr)
 {
     m_window = std::make_unique<Window_Win32>(create_info.window_create_info);
     d3d12::create_d3d12_context(create_info.context_create_info, &m_d3d12_context);
+    m_swapchain = std::make_unique<Swapchain>(m_window.get(), &m_d3d12_context);
+    m_resource_manager = std::make_unique<Resource_Manager>(create_info.resource_manager_create_info, &m_d3d12_context);
 }
 
 Sample_Application::~Sample_Application()
 {
     d3d12::await_context(&m_d3d12_context);
+    m_swapchain = nullptr;
+    m_resource_manager = nullptr;
     d3d12::destroy_d3d12_context(&m_d3d12_context);
 }
 
 void Sample_Application::run()
 {
-    m_running = true;
     auto current_time = std::chrono::system_clock::now();
     auto last_time = current_time;
     while (m_window->get_data().is_alive)
     {
+        m_window->poll_events();
+        m_swapchain->resize_if_size_changed();
+
         using Duration = std::chrono::duration<double>;
         auto delta_time = std::chrono::duration_cast<Duration>(current_time - last_time).count();
 
--- src/core_sample_framework/sample_base_app.hpp
+++ src/core_sample_framework/sample_base_app.hpp
@@ -2,6 +2,7 @@
 
 #include "d3d12/d3d12_device.hpp"
 #include "resource.hpp"
+#include "swapchain.hpp"
 #include "window.hpp"
 
 #include <memory>
@@ -12,6 +13,7 @@ struct Sample_Application_Create_Info
 {
     Window_Create_Info window_create_info;
     d3d12::D3D12_Context_Create_Info context_create_info;
+    Resource_Manager_Create_Info resource_manager_create_info;
 };
 
 class Sample_Application
@@ -26,13 +28,13 @@ public:
     virtual void update_gui() noexcept {};
     virtual void render(ID3D12GraphicsCommandList9* cmd, double delta_time) noexcept = 0;
 
-private:
+protected:
     void render_gui(ID3D12GraphicsCommandList9* cmd) noexcept;
 
 protected:
-    bool m_running;
     std::unique_ptr<Window> m_window;
     d3d12::D3D12_Context m_d3d12_context;
-    Resource_Manager m_resource_manager;
+    std::unique_ptr<Swapchain> m_swapchain;
+    std::unique_ptr<Resource_Manager> m_resource_manager;
 };
 }
