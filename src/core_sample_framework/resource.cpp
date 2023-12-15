164aa393984e02b072aec959cbbb73e816778b27
Robert <33040161+rtryan98@users.noreply.github.com>
2023-12-15 07:29:58 +0100

    Initial Resource Manager

--- src/core_sample_framework/resource.cpp
+++ src/core_sample_framework/resource.cpp
@@ -1,6 +1,78 @@
 #include "resource.hpp"
+#include "d3d12/d3d12_device.hpp"
+
+#include <D3D12MemAlloc.h>
 
 namespace core::sf
 {
+Resource_Manager::Resource_Manager(const Resource_Manager_Create_Info& create_info, d3d12::D3D12_Context* d3d12_context) noexcept
+    : m_d3d12_context(d3d12_context)
+    , m_resource_descriptor_heap(nullptr)
+    , m_sampler_descriptor_heap(nullptr)
+    , m_buffers()
+    , m_textures()
+    , m_shaders()
+    , m_graphics_pipelines()
+    , m_compute_pipelines()
+    , m_allocator(nullptr)
+{
+    m_buffers.reserve(create_info.max_buffer_count);
+    m_textures.reserve(create_info.max_texture_count);
+    m_shaders.reserve(create_info.max_shader_count);
+    m_graphics_pipelines.reserve(create_info.max_graphics_pipeline_count);
+    m_compute_pipelines.reserve(create_info.max_compute_pipeline_count);
+
+    D3D12MA::ALLOCATOR_DESC allocator_desc = {
+        .Flags = D3D12MA::ALLOCATOR_FLAG_SINGLETHREADED,
+        .pDevice = d3d12_context->device,
+        .PreferredBlockSize = 0,
+        .pAllocationCallbacks = nullptr,
+        .pAdapter = d3d12_context->adapter
+    };
+    D3D12MA::CreateAllocator(&allocator_desc, &m_allocator);
+}
+
+Resource_Manager::~Resource_Manager()
+{
+    for (auto& buffer : m_buffers)
+    {
+        if (!buffer.alive)
+            continue;
+        // destroy buffer
+    }
+    m_allocator->Release();
+}
+
+Buffer* Resource_Manager::create_buffer(const Buffer_Create_Info & create_info) noexcept
+{
+    auto* buffer =  &m_buffers.emplace_back();
+    buffer->alive = true;
+
+    D3D12MA::ALLOCATION_DESC alloc_desc = {
+        .Flags = D3D12MA::ALLOCATION_FLAG_NONE,
+        .HeapType = create_info.heap_type,
+        .ExtraHeapFlags = D3D12_HEAP_FLAG_NONE,
+        .CustomPool = nullptr,
+        .pPrivateData = nullptr
+    };
+    D3D12_RESOURCE_DESC1 resource_desc = {
+        .Dimension = D3D12_RESOURCE_DIMENSION_BUFFER,
+        .Alignment = 0ull,
+        .Width = create_info.size,
+        .Height = 1,
+        .DepthOrArraySize = 1,
+        .MipLevels = 1,
+        .Format = DXGI_FORMAT_UNKNOWN,
+        .SampleDesc = { .Count = 1, .Quality = 0 },
+        .Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR,
+        .Flags = D3D12_RESOURCE_FLAG_NONE,
+        .SamplerFeedbackMipRegion = {}
+    };
+    m_allocator->CreateResource3(&alloc_desc, &resource_desc,
+        D3D12_BARRIER_LAYOUT_UNDEFINED, nullptr, 0, nullptr,
+        &buffer->allocation, IID_PPV_ARGS(&buffer->resource));
+
+    return buffer;
+}
 
 }
--- src/core_sample_framework/resource.hpp
+++ src/core_sample_framework/resource.hpp
@@ -2,15 +2,42 @@
 
 #include <cstdint>
 #include <d3d12.h>
+#include <d3d12/d3d12_shader.hpp>
+#include <string>
+#include <vector>
+
+namespace D3D12MA
+{
+class Allocation;
+class Allocator;
+}
+
+namespace core::d3d12
+{
+struct D3D12_Context;
+}
 
 namespace core::sf
 {
-struct Base_GPU_Resource
+struct Base_Resource
+{
+    bool alive;
+};
+
+struct Base_GPU_Resource : public Base_Resource
 {
     uint32_t bindless_index;
     DXGI_FORMAT format;
     D3D12_CPU_DESCRIPTOR_HANDLE cpu_descriptor;
     D3D12_GPU_DESCRIPTOR_HANDLE gpu_descriptor;
+    D3D12MA::Allocation* allocation;
+    ID3D12Resource2* resource;
+};
+
+struct Buffer_Create_Info
+{
+    uint64_t size;
+    D3D12_HEAP_TYPE heap_type;
 };
 
 struct Buffer : public Base_GPU_Resource
@@ -18,24 +45,95 @@ struct Buffer : public Base_GPU_Resource
 
 };
 
+struct Texture_Create_Info
+{
+
+};
+
 struct Texture : public Base_GPU_Resource
 {
-    D3D12_BARRIER_LAYOUT current_layout;
-};
-
-struct Shader
-{
 
 };
 
-struct Graphics_Pipeline
+struct Shader : public Base_Resource
 {
+    d3d12::D3D12_Shader_Workgroups workgroups;
+    D3D12_SHADER_BYTECODE bytecode;
+    std::string relative_path;
+};
 
+struct Base_Pipeline : public Base_Resource
+{
+    ID3D12PipelineState* pso;
+};
+
+struct Graphics_Pipeline_Create_Info
+{
+    struct
+    {
+        Shader* vs;
+        Shader* ds;
+        Shader* hs;
+        Shader* gs;
+    } standard_stages;
+    struct
+    {
+        Shader* as;
+        Shader* ms;
+    } mesh_shading_stages;
+    Shader* ps;
+};
+
+struct Graphics_Pipeline : public Base_Pipeline
+{
+    Shader* vs;
+    Shader* ds;
+    Shader* hs;
+    Shader* gs;
+    Shader* as;
+    Shader* ms;
+    Shader* ps;
+};
+
+struct Compute_Pipeline : public Base_Pipeline
+{
+    Shader* cs;
+};
+
+struct Resource_Manager_Create_Info
+{
+    uint32_t max_buffer_count = 65536;
+    uint32_t max_texture_count = 65536;
+    uint32_t max_shader_count = 65536;
+    uint32_t max_graphics_pipeline_count = 65536;
+    uint32_t max_compute_pipeline_count = 65536;
 };
 
 class Resource_Manager
 {
+public:
+    explicit Resource_Manager(const Resource_Manager_Create_Info& create_info, d3d12::D3D12_Context* d3d12_context) noexcept;
+    ~Resource_Manager();
 
+    Buffer* create_buffer(const Buffer_Create_Info& create_info) noexcept;
+    Texture* create_texture(const Texture_Create_Info& create_info) noexcept;
+    Shader* create_shader() noexcept;
+    Graphics_Pipeline* create_graphics_pipeline(const Graphics_Pipeline_Create_Info& create_info) noexcept;
+    Compute_Pipeline* create_compute_pipeline(Shader* shader) noexcept;
+
+private:
+    d3d12::D3D12_Context* m_d3d12_context;
+
+    ID3D12DescriptorHeap* m_resource_descriptor_heap;
+    ID3D12DescriptorHeap* m_sampler_descriptor_heap;
+
+    std::vector<Buffer> m_buffers;
+    std::vector<Texture> m_textures;
+    std::vector<Shader> m_shaders;
+    std::vector<Graphics_Pipeline> m_graphics_pipelines;
+    std::vector<Compute_Pipeline> m_compute_pipelines;
+
+    D3D12MA::Allocator* m_allocator;
 };
 
 }
