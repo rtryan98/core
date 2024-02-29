ab426271fbac46bc19f6787cebf070d5e2619056
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2024-02-29 12:45:33 +0100

    update bitmask operators to return the correct type

--- src/core/core/common/bitmask.hpp
+++ src/core/core/common/bitmask.hpp
@@ -11,26 +11,26 @@ template<typename T>
 requires CORE_ENABLE_BIT_OPERATORS<T>
 constexpr auto operator&(T left, T right) noexcept
 {
-    return std::to_underlying(left) & std::to_underlying(right);
+    return T(std::to_underlying(left) & std::to_underlying(right));
 }
 
 template<typename T>
 requires CORE_ENABLE_BIT_OPERATORS<T>
 constexpr auto operator|(T left, T right) noexcept
 {
-    return std::to_underlying(left) | std::to_underlying(right);
+    return T(std::to_underlying(left) | std::to_underlying(right));
 }
 
 template<typename T>
 requires CORE_ENABLE_BIT_OPERATORS<T>
 constexpr auto operator^(T left, T right) noexcept
 {
-    return std::to_underlying(left) ^ std::to_underlying(right);
+    return T(std::to_underlying(left) ^ std::to_underlying(right));
 }
 
 template<typename T>
 requires CORE_ENABLE_BIT_OPERATORS<T>
 constexpr auto operator~(T value) noexcept
 {
-    return ~std::to_underlying(value);
+    return T(~std::to_underlying(value));
 }
