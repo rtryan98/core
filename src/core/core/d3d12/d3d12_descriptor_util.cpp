071bf2f0d329b35a2847e3bcd6a08adad6b9975a
Robert <33040161+rtryan98@users.noreply.github.com>
2024-03-22 06:22:34 +0100

    more descriptor functions

--- src/core/core/d3d12/d3d12_descriptor_util.cpp
+++ src/core/core/d3d12/d3d12_descriptor_util.cpp
@@ -37,42 +37,61 @@ D3D12_SHADER_RESOURCE_VIEW_DESC make_full_texture_srv(
     D3D12_SRV_DIMENSION srv_dimension,
     uint32_t depth_or_array_layers)
 {
+    return make_texture_srv(
+        format,
+        srv_dimension,
+        0,
+        depth_or_array_layers,
+        0,
+        ~0u,
+        D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING);
+}
+
+D3D12_SHADER_RESOURCE_VIEW_DESC make_texture_srv(
+    DXGI_FORMAT format,
+    D3D12_SRV_DIMENSION srv_dimension,
+    uint32_t first_array_index,
+    uint32_t depth_or_array_layers,
+    uint32_t first_mip_level,
+    uint32_t mip_levels,
+    uint32_t shader_4_component_mapping)
+{
     D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc = {
         .Format = format,
         .ViewDimension = srv_dimension,
-        .Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING
+        .Shader4ComponentMapping = shader_4_component_mapping
     };
 
     switch (srv_dimension)
     {
     case D3D12_SRV_DIMENSION_TEXTURE1D:
         srv_desc.Texture1D = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
             .ResourceMinLODClamp = 0.0f
         };
         break;
     case D3D12_SRV_DIMENSION_TEXTURE1DARRAY:
         srv_desc.Texture1DArray = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
-            .FirstArraySlice = 0,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers,
             .ResourceMinLODClamp = 0.0f
         };
         break;
     case D3D12_SRV_DIMENSION_TEXTURE2D:
         srv_desc.Texture2D = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
             .ResourceMinLODClamp = 0.0f
         };
         break;
     case D3D12_SRV_DIMENSION_TEXTURE2DARRAY:
         srv_desc.Texture2DArray = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
-            .FirstArraySlice = 0,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers,
             .ResourceMinLODClamp = 0.0f
         };
@@ -84,30 +103,30 @@ D3D12_SHADER_RESOURCE_VIEW_DESC make_full_texture_srv(
         break;
     case D3D12_SRV_DIMENSION_TEXTURE2DMSARRAY:
         srv_desc.Texture2DMSArray = {
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
     case D3D12_SRV_DIMENSION_TEXTURE3D:
         srv_desc.Texture3D = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
             .ResourceMinLODClamp = 0.0f
         };
         break;
     case D3D12_SRV_DIMENSION_TEXTURECUBE:
         srv_desc.TextureCube = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
             .ResourceMinLODClamp = 0.0f
         };
         break;
     case D3D12_SRV_DIMENSION_TEXTURECUBEARRAY:
         srv_desc.TextureCubeArray = {
-            .MostDetailedMip = 0,
-            .MipLevels = ~0u,
-            .First2DArrayFace = 0,
-            .NumCubes = depth_or_array_layers / 6u,
+            .MostDetailedMip = first_mip_level,
+            .MipLevels = mip_levels,
+            .First2DArrayFace = first_array_index,
+            .NumCubes = depth_or_array_layers / 6u, // TODO: this might be wrong`with first_array_index = 0
             .ResourceMinLODClamp = 0.0f
         };
         break;
@@ -121,6 +140,17 @@ D3D12_SHADER_RESOURCE_VIEW_DESC make_full_texture_srv(
 D3D12_UNORDERED_ACCESS_VIEW_DESC make_full_texture_uav(
     DXGI_FORMAT format,
     D3D12_UAV_DIMENSION uav_dimension,
+    uint32_t depth_or_array_layers,
+    uint32_t mip_slice,
+    uint32_t plane_slice)
+{
+    return make_texture_uav(format, uav_dimension, 0, depth_or_array_layers, mip_slice, 0);
+}
+
+D3D12_UNORDERED_ACCESS_VIEW_DESC make_texture_uav(
+    DXGI_FORMAT format,
+    D3D12_UAV_DIMENSION uav_dimension,
+    uint32_t first_array_index,
     uint32_t depth_or_array_layers,
     uint32_t mip_slice,
     uint32_t plane_slice)
@@ -140,7 +170,7 @@ D3D12_UNORDERED_ACCESS_VIEW_DESC make_full_texture_uav(
     case D3D12_UAV_DIMENSION_TEXTURE1DARRAY:
         uav_desc.Texture1DArray = {
             .MipSlice = mip_slice,
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
@@ -153,7 +183,7 @@ D3D12_UNORDERED_ACCESS_VIEW_DESC make_full_texture_uav(
     case D3D12_UAV_DIMENSION_TEXTURE2DARRAY:
         uav_desc.Texture2DArray = {
             .MipSlice = mip_slice,
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers,
             .PlaneSlice = plane_slice,
         };
@@ -165,7 +195,7 @@ D3D12_UNORDERED_ACCESS_VIEW_DESC make_full_texture_uav(
         break;
     case D3D12_UAV_DIMENSION_TEXTURE2DMSARRAY:
         uav_desc.Texture2DMSArray = {
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
@@ -190,6 +220,17 @@ D3D12_RENDER_TARGET_VIEW_DESC make_full_texture_rtv(
     uint32_t mip_slice,
     uint32_t plane_slice)
 {
+    return make_texture_rtv(format, rtv_dimension, 0, depth_or_array_layers, mip_slice, plane_slice);
+}
+
+D3D12_RENDER_TARGET_VIEW_DESC make_full_texture_rtv(
+    DXGI_FORMAT format,
+    D3D12_RTV_DIMENSION rtv_dimension,
+    uint32_t first_array_index,
+    uint32_t depth_or_array_layers,
+    uint32_t mip_slice,
+    uint32_t plane_slice)
+{
     D3D12_RENDER_TARGET_VIEW_DESC rtv_desc = {
         .Format = format,
         .ViewDimension = rtv_dimension,
@@ -205,7 +246,7 @@ D3D12_RENDER_TARGET_VIEW_DESC make_full_texture_rtv(
     case D3D12_RTV_DIMENSION_TEXTURE1DARRAY:
         rtv_desc.Texture1DArray = {
             .MipSlice = mip_slice,
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
@@ -218,7 +259,7 @@ D3D12_RENDER_TARGET_VIEW_DESC make_full_texture_rtv(
     case D3D12_RTV_DIMENSION_TEXTURE2DARRAY:
         rtv_desc.Texture2DArray = {
             .MipSlice = mip_slice,
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers,
             .PlaneSlice = plane_slice,
         };
@@ -230,7 +271,7 @@ D3D12_RENDER_TARGET_VIEW_DESC make_full_texture_rtv(
         break;
     case D3D12_RTV_DIMENSION_TEXTURE2DMSARRAY:
         rtv_desc.Texture2DMSArray = {
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
@@ -254,6 +295,16 @@ D3D12_DEPTH_STENCIL_VIEW_DESC make_full_texture_dsv(
     uint32_t depth_or_array_layers,
     uint32_t mip_slice)
 {
+    return make_texture_dsv(format, dsv_dimension, 0, depth_or_array_layers, mip_slice);
+}
+
+D3D12_DEPTH_STENCIL_VIEW_DESC make_texture_dsv(
+    DXGI_FORMAT format,
+    D3D12_DSV_DIMENSION dsv_dimension,
+    uint32_t first_array_index,
+    uint32_t depth_or_array_layers,
+    uint32_t mip_slice)
+{
     D3D12_DEPTH_STENCIL_VIEW_DESC dsv_desc = {
         .Format = format,
         .ViewDimension = dsv_dimension,
@@ -269,7 +320,7 @@ D3D12_DEPTH_STENCIL_VIEW_DESC make_full_texture_dsv(
     case D3D12_DSV_DIMENSION_TEXTURE1DARRAY:
         dsv_desc.Texture1DArray = {
             .MipSlice = mip_slice,
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
@@ -281,7 +332,7 @@ D3D12_DEPTH_STENCIL_VIEW_DESC make_full_texture_dsv(
     case D3D12_DSV_DIMENSION_TEXTURE2DARRAY:
         dsv_desc.Texture2DArray = {
             .MipSlice = mip_slice,
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
@@ -292,7 +343,7 @@ D3D12_DEPTH_STENCIL_VIEW_DESC make_full_texture_dsv(
         break;
     case D3D12_DSV_DIMENSION_TEXTURE2DMSARRAY:
         dsv_desc.Texture2DMSArray = {
-            .FirstArraySlice = 0,
+            .FirstArraySlice = first_array_index,
             .ArraySize = depth_or_array_layers
         };
         break;
